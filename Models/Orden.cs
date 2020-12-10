95bc440f4a3944c23d4771e29823969c06c2ce98
jbanos2000 <71935605+jbanos2000@users.noreply.github.com>
2020-12-10 16:31:15 -0500

    Creando Orden Model

---
+++ Models/Orden.cs
@@ -0,0 +1,14 @@
+//Orden de compra, detalles de productos Id, Precio y Cantidad
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectProgra2020.Models
+{
+    [Table("t_orden")]
+    
+    public class Orden
+    {
+    }
+}
+    
