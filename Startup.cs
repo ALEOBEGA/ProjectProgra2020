66cb655f5360203845f32958177ddf2da65b1ded
jbanos2000 <71935605+jbanos2000@users.noreply.github.com>
2020-12-18 17:14:29 -0500

    Sin redirecionamiento de http

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,7 @@ namespace ProjectProgra2020
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
